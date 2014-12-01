51569ebadc3b043720dee4d10486d1aa208855d8
Jan Grodowski <jgrodowski@gmail.com>
2014-12-01 19:53:02 +0100

    validate input

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,22 @@
 class FizzBuzz  
-
+  FIZZ = 'fizz'
+  
+  def call(num)
+    @num = num
+    validate!
+    return FIZZ if num.to_i == 3
+    num.to_s
+  end
+  
+  private 
+  
+  def validate!
+    raise 'invalid input' unless valid?
+  end
+  
+  def valid?
+    @num.respond_to?(:to_i) && 
+    @num.to_i == @num &&
+    @num.to_i > 0
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,23 @@
 require 'fizzbuzz'
 
 describe FizzBuzz do 
+  it 'acceps a number and returns it' do 
+    expect(subject.call(1)).to eq '1'
+  end
 
+  it 'returns fizz for 3' do 
+    expect(subject.call(3)).to eq 'fizz'
+  end  
+  
+  it 'raises an error for 0 and -2' do 
+    [0, -2].each do |n|
+      expect { subject.call(n) }.to raise_error('invalid input')
+    end
+  end
+  
+  it 'raises an error for any non-integer input' do 
+    [1.22, 'test', {}].each do |n|
+      expect { subject.call(n) }.to raise_error('invalid input')
+    end
+  end
 end
